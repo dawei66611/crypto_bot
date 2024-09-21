5deb119b6489955aa18dc066c5fdcfc44158d149
dawei66611 <dawei66611@gmail.com>
2024-09-21 19:46:03 +0800

    Update indicators.py

--- utils/indicators.py
+++ utils/indicators.py
@@ -1,3 +1,5 @@
+# utils/indicators.py
+
 import numpy as np
 import talib
 
@@ -5,10 +7,13 @@ def calculate_technical_indicators(prices):
     prices = np.array(prices)
     macd, macd_signal, macd_hist = talib.MACD(prices, fastperiod=12, slowperiod=26, signalperiod=9)
     rsi = talib.RSI(prices, timeperiod=14)
+    upper, middle, lower = talib.BBANDS(prices, timeperiod=20)
 
     return {
         'MACD': macd[-1],
         'MACD_Signal': macd_signal[-1],
-        'RSI': rsi[-1]
+        'RSI': rsi[-1],
+        'Bollinger_Upper': upper[-1],
+        'Bollinger_Middle': middle[-1],
+        'Bollinger_Lower': lower[-1]
     }
-
