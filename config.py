c444e8d4ec33123f5d0ce845cd4019503c646c4f
dawei66611 <dawei66611@gmail.com>
2024-09-21 19:47:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# config.py
+
 import os
 from dotenv import load_dotenv
 
@@ -10,7 +12,10 @@ NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 CMC_API_KEY = os.getenv('CMC_API_KEY')
 GROUP_CHAT_ID = int(os.getenv('GROUP_CHAT_ID', '0'))
 CHANNEL_CHAT_ID = int(os.getenv('CHANNEL_CHAT_ID', '0'))
+DATABASE = os.getenv('DATABASE', 'bot_database.db')
 
 # GPT模型名称
 GPT_MODEL = "gpt-4"
 
+# 有效激活码列表（可在 .env 中配置）
+VALID_ACTIVATION_CODES = os.getenv('VALID_ACTIVATION_CODES', '').split(',')
