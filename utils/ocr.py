0f987aad9c2f346011c728d996cb7eb64050c6d0
dawei66611 <dawei66611@gmail.com>
2024-09-21 19:09:50 +0800

    Update ocr.py

--- utils/ocr.py
+++ utils/ocr.py
@@ -1,1 +1,9 @@
+import pytesseract
+from PIL import Image
+import io
+
+def perform_ocr(image_data):
+    image = Image.open(io.BytesIO(image_data))
+    text = pytesseract.image_to_string(image)
+    return text
 
